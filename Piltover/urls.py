05e31f7fb2b374563737f3110f054423e0d2b53f
gems <urgems000@gmail.com>
2020-11-04 21:08:56 +0900

    [modify] exempt CSRF

--- Piltover/urls.py
+++ Piltover/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('velkoz/', GraphQLView.as_view(graphiql=True)),
+    path('velkoz/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
