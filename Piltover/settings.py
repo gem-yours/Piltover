f83075669a03572cc5d42893612aa07a6fea7c19
gems <urgems000@gmail.com>
2021-06-28 01:59:30 +0900

    [add] make it view content files

--- Piltover/settings.py
+++ Piltover/settings.py
@@ -129,3 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# media files (user uploaded images)
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/media'
--- Piltover/urls.py
+++ Piltover/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from django.urls import path
-from graphene_django.views import GraphQLView
+from django.urls import path, re_path
+from django.conf import settings
+from django.views.static import serve
 from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('velkoz/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+]
+
+urlpatterns += [
+    re_path('^' + settings.MEDIA_URL.lstrip('/') + r'(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+        'show_indexes': settings.DEBUG
+    }),
 ]
